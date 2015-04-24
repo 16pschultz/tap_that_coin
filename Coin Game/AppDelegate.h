90358f480751def5288cf0b8c8358149c6021f37
Peter Schultz <Peter@Peters-MacBook-Pro.local>
2015-04-24 09:08:46 -0400

    Initial Commit

---
+++ Coin Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Coin Game
+//
+//  Created by Peter Schultz on 4/24/15.
+//  Copyright (c) 2015 Peter Schultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Coin Game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Coin Game
+//
+//  Created by Peter Schultz on 4/24/15.
+//  Copyright (c) 2015 Peter Schultz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
